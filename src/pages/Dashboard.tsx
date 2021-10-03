69905d7257e71d83025088cf5a832808c8b23df7
Andykofi <andykofi2@gmail.com>
2021-10-03 21:42:04 +0100

    first commit

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Wrapper from "../components/Wrapper";
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <Wrapper>
+                <h1>The Cat Dashboard</h1>
+            </Wrapper>
+        );
+    }
+}
+
+export default Dashboard;
---
+++ src/pages/ImageUpload.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Wrapper from "../components/Wrapper";
+
+const Upload = () => {
+    return (
+        <Wrapper>
+            Cat Upload page
+        </Wrapper>
+    );
+};
+
+export default Upload;
